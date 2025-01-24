6070fd48803ce3f4e27fb64b167f081a49a3db3d
amzakaria94 <freedomblackknights@gmail.com>
2025-01-24 17:37:11 +0200

    updating DataBase

--- DAL/Migrations/20250124153158_init.cs
+++ DAL/Migrations/20250124153158_init.cs
@@ -5,7 +5,7 @@
 namespace DAL.Migrations
 {
     /// <inheritdoc />
-    public partial class addBrand : Migration
+    public partial class init : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
