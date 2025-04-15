611560eec7fcca1282e4b2cead73494e707d3a32
Ahmed Zakaria <59110684+amzakaria94@users.noreply.github.com>
2025-04-15 14:37:09 +0200

    Delete E_Commerce_MVC directory

--- E_Commerce_MVC/Models/ErrorViewModel.cs
+++
@@ -1,7 +0,0 @@
-namespace E_Commerce_MVC.Models {
-	public class ErrorViewModel {
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
--- E_Commerce_MVC/Models/UserViewModel/LoginViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace E_Commerce_MVC.Models.UserViewModel
-{
-	public class LoginViewModel
-	{
-		public string UserName { get; set; }
-		public string Password { get; set; }
-		public bool RememberMe { get; set; }
-	}
-}
