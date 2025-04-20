2dec849f4b5e6f851a5b88f1ba2f24a98ab5bf57
amzakaria94 <freedomblackknights@gmail.com>
2025-04-20 05:18:08 +0200

    Adding the FrontLayer

---
+++ E_Commerce_MVC/Models/EntitiesViewModel/Store.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_Commerce_MVC.Models.EntitiesViewModel
+{
+	public class Store
+	{
+		[Key]
+		public int StoreId { get; set; }
+
+		[Required]
+		public string? StoreName { get; set; }
+
+		public bool b_deleted { get; set; }
+	}
+}
